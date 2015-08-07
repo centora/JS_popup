7fda349df4e622cb2573b35ab36595482fb3dace
Centora <oksana.blonskaya@gmail.com>
2015-08-07 07:37:20 +0300

    Develop js for popup [0.0.1]

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
 (function(){
+	'use strict';
 
-  
+  // Show - method
+  // Hide - method
+  // Event buttons - method get 2 params, 1 - event, 2 - fn
+  // AppendTo  (params - element in which we want append our popup)
+
+  var popup = {
+  	show: function(){
+
+  	},
+  	hide: function(){
+
+  	},
+  	addEventBtn: function(event, fn){
+
+  	},
+  	appendTo: function(selector){
+
+  	}
+  }
+
+  var helper = {
+  	$: function( selector, context ){
+  		return (context || document).querySelectorAll( selector );
+  	},
+  	hide: function(elem){
+  		elem.style.display = "none";
+  	},
+  	show: function(elem){
+  		elem.style.display = "block";
+  	}
+  }
+
+   var p = helper.$(".popup")[0];
+   helper.hide(p);
+   console.log(helper.$("h2", p));
+
 
 })();
